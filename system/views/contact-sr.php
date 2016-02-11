f32d7d062e345fe56c044f48c4997883623a30ad
Milos <mzarkovicm@gmail.com>
2016-02-11 16:29:15 +0100

    ubacen PHPMailer

--- system/views/contact-sr.php
+++ system/views/contact-sr.php
@@ -4,6 +4,7 @@
 	<b>Mobilni tel.:</b> +38163 453 835<br>
 	<b>Adresa:</b> Ustanička 12 ulaz 1, 11000 Beograd<br>
 	<b>E mail:</b> <img src="http://www.advokatmarinkovic.rs/wp-content/uploads/2014/08/mail-w.png" alt="mail-w" width="211" height="14" class="email-img">
+	<?php echo $msg; ?>
 	<form action="/sr/kontakt" method="post">
 		<?php generate_form_field("text", "name", "Vaše ime", "", $errors, "Vaše ime"); ?>
 		<?php generate_form_field("text", "email", "Vaš email", "", $errors, "Vaša email adresa"); ?>
