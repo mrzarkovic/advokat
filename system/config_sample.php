90e935241db8f4d91c2264b67ccc0ad0134337d2
Milos <mzarkovicm@gmail.com>
2016-02-12 13:50:10 +0100

    config added

---
+++ system/config_sample.php
@@ -0,0 +1,10 @@
+<?php
+// Database
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "attorney");
+
+// PHPMailer
+define("EMAIL", "mzarkovicm@gmail.com");
+define("EMAIL_PASSWORD", "jdadtfvehsrvwicy");
